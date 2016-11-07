cfa1be05e36957a1a3a2970f15751cb315f0be9a
Daniel Fisher <danielfisher0@gmail.com>
2016-11-07 11:27:51 +0000

    changed epoch

--- src/data/config.py
+++ src/data/config.py
@@ -2,8 +2,8 @@ import numpy as np
 import countries
 
 
-myd = {'year': 2012,
-       'doy_range': np.arange(267, 300, 1),  # 267-300
+myd = {'year': 2014,
+       'doy_range': np.arange(92, 153, 1),  # 267-300
        'min_time': 1600,
        'max_time': 2200,
        'min_fires': 10,  # minimum number of fires in the scene
