5284f9a48c4c58801673809e5607725258d5a697
dabillox <danielfisher0@gmail.com>
2017-02-10 15:33:29 +0000

    think it is nearly there for some first testing

--- src/orac/run_modis_master.py
+++ src/orac/run_modis_master.py
@@ -5,33 +5,27 @@ import re
 import glob
 
 # TODO set up iterator to loop over a list of modis files
+# Hard code in the paths and the files for now
+mod_dir = '/testinput/'
+l1name = 'MYD021KM.A2008172.0405.005.2009317014309.hdf'
+pixel_limit = "800 900 1000 1100"
+yr, doy, hr, mn = '2008', '172', '.04', '05'
 
-# Set-up the paths and extract the date-time from the input file name
-l1path = sys.argv[1]
-l1name = os.path.basename(l1path)
-l1dir = os.path.dirname(l1path)
-
-nrt_ws = '/group_workspaces/cems/nrt_ecmwf_metop'
-rsg_ws = '/group_workspaces/cems/rsgnceo'
-nceo_ws = '/group_workspaces/cems2/nceo_generic'
-orac_dir = '/home/users/gethomas/orac_code'
+out_dir = '/testoutput/modis_testing/'
+predir = out_dir + 'pre/'
+maindir = out_dir + 'main/'
+postdir = out_dir
 
 cldsaddir = '/group_workspaces/cems/cloud_ecv/orac/sad_dir'
 cldphs = ['WAT', 'ICE']
 aersaddir = '/group_workspaces/cems/nceo_aerosolfire/gethomas/luts'
 aerphs = ['AMZ', 'BOR', 'CER', 'AFR']  # Amazon, Boreal, Cerrado, Africa
-pixel_limit = "800 900 1000 1100"
-
-# Ensure ORAC libaries are able to be found
-os.putenv('LD_LIBRARY_PATH', os.getenv('LD_LIBRARY_PATH') + ':'
-          + orac_dir + '/libraries/lib')
 
 # Call the pre-processing
-os.chdir(orac_dir + '/trunk/tools')
 print " **** Calling ORAC preprocessing on file: ****"
 print "   " + mod_dir + '/' + l1name
 pre_cmd = mod_dir + '/' + l1name \
-          + ' -o ' + preoutdir \
+          + ' -o ' + predir \
           + ' --batch ' \
           + ' --limit ' + pixel_limit
 
@@ -39,15 +33,13 @@ os.system('./orac_preproc.py ' + pre_cmd)
 
 # Call the processing for the desired phases/types
 # Path to the preprocessed files
-msi_root = glob.glob(preoutdir + '/*_' + yr + mth + day + hr + mn + '_*.msi.nc')
+msi_root = glob.glob(predir + '/*_' + yr + doy + hr + mn + '_*.msi.nc')
 msi_root = os.path.basename(msi_root=msi_root[:len(msi_root) - 7])
 
 # Call main first time  TODO Set channels to the right ones
-proc_cmd = '-i ' + preoutdir \
-           + ' -o ' + prooutdir \
-           + ' --orac_dir ' + orac_dir + '/trunk' \
+proc_cmd = '-i ' + predir \
+           + ' -o ' + maindir \
            + ' --sad_dir ' + cldsaddir \
-           + ' --use_channel 1 1 1 0 0 1 1' \
            + ' --phase '
 
 for phs in cldphs:
@@ -56,13 +48,9 @@ for phs in cldphs:
               + msi_root)
 
 # Call main for aerosol phase, does this need to be done for MODIS?  Also set channels correctly
-proc_cmd = '-i ' + preoutdir \
-           + ' -o ' + prooutdir \
-           + ' --orac_dir ' + orac_dir + '/trunk' \
+proc_cmd = '-i ' + predir \
+           + ' -o ' + maindir \
            + ' --sad_dir ' + aersaddir \
-           + ' --use_channel 1 1 1 0 0 0 0' \
-           + ' -a AppCld1L' \
-           + ' --ret_class ClsAerOx' \
            + ' --phase '
 #        +' --extra_lines /home/users/gethomas/orac_code/aerosol_scripts_py/xtra_driver_lines.txt' \
 
@@ -72,9 +60,8 @@ for phs in aerphs:
               + msi_root)
 
 # Call the post-processor
-post_cmd = '-i ' + prooutdir + \
-           ' -o ' + postoutdir \
-           + ' --orac_dir ' + orac_dir + '/trunk' \
+post_cmd = '-i ' + maindir + \
+           ' -o ' + postdir \
            + '--verbose' \
            + ' --compress ' + msi_root \
            + ' --phases ' + ' '.join(aerphs) + ' ' + ' '.join(cldphs)
