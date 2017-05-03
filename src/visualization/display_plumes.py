8983c4094ab80a62032910bc04db90c3ddebc414
Daniel Fisher <danielfisher0@gmail.com>
2017-05-03 15:08:04 +0100

    now also reading in vis cahnnels to show the plume

--- src/visualization/display_plumes.py
+++ src/visualization/display_plumes.py
@@ -14,27 +14,44 @@ import scipy.ndimage as ndimage
 
 from matplotlib.path import Path
 from netCDF4 import Dataset
+from pyhdf.SD import SD, SDC
 from datetime import datetime
 
-def get_sub_df(primary_file, mask_df):
+
+def get_primary_time(primary_file):
     tt = datetime.strptime(primary_file.split('_')[-2], '%Y%m%d%H%M').timetuple()
     primary_datestring = str(tt.tm_year) + \
                          str(tt.tm_yday).zfill(3) + \
                          '.' + \
                          str(tt.tm_hour).zfill(2) + \
                          str(tt.tm_min).zfill(2)
-    return mask_df[mask_df['filename'].str.contains(primary_datestring)]
+    return primary_datestring
+
+
+def get_sub_df(primary_time, mask_df):
+    return mask_df[mask_df['filename'].str.contains(primary_time)]
+
+
+def read_vis(l1b_file):
+    ds = SD(l1b_file, SDC.READ)
+    mod_params_ref = ds.select("EV_1KM_RefSB").attributes()
+    ref = ds.select("EV_1KM_RefSB").get()
+    ref_chan = 0
+    vis = (ref[ref_chan, :, :] - mod_params_ref['radiance_offsets'][ref_chan]) * mod_params_ref['radiance_scales'][
+        ref_chan]
+    vis = np.round((vis * (255 / np.max(vis))) * 1).astype('uint8')
+    return vis
 
 
 def open_primary(primary_file):
     return Dataset(primary_file)
 
 
-def make_mask(primary_data, primary_file, mask_df):
+def make_mask(primary_data, primary_time, mask_df):
     primary_shape = primary_data.variables['cer'].shape
 
     # get teh sub dataframe associated with the mask
-    sub_df = get_sub_df(primary_file, mask_df)
+    sub_df = get_sub_df(primary_time, mask_df)
 
     # create grid to hold the plume mask
     nx = primary_shape[1]
@@ -67,14 +84,28 @@ def label_plumes(mask):
     return plume_positions
 
 
-def make_plot(primary_data, plume_mask):
-    pass
+def make_plot(visrad, primary_data, plume_positions):
+
+    # iterate over the plumes
+    for pp in plume_positions:
+
+        fig, axes = plt.subplots(2, 3)
+
+        for ax, k in zip(axes.flatten(), ['' , 'cer', 'cot', 'ctp', 'ctt', 'costjm']):
+            if not k:
+                ax.imshow(visrad[pp[0]:pp[1], pp[2]:pp[3]], cmap='gray', interpolation='None')
+            else:
+                data = primary_data.variables[k][pp[0]:pp[1], pp[2]:pp[3]]
+                p = ax.imshow(data)
+                #cbar = plt.colorbar(p, ax=ax)
+        plt.show()
 
 
 def main():
 
 
     # set up paths
+    l1b_path = '../../data/raw/l1b/'
     orac_data_path = '../../data/processed/orac_test_scene/'
     mask_path = '../../data/interim/myd021km_plumes_df.pickle'
     output = ''
@@ -85,17 +116,24 @@ def main():
     # iterate over modis files
     for primary_file in glob.glob(orac_data_path + '*/*/*primary*'):
 
+        # first get the primary file time
+        primary_time = get_primary_time(primary_file)
+
+        # find and read the vis channel of the associated l1b file
+        l1b_file = glob.glob(l1b_path + '*' + primary_time + '*')[0]
+        visrad = read_vis(l1b_file)
+
         # open up the ORAC primary file
         primary_data = open_primary(primary_file)
 
         # make the smoke plume mask
-        plume_mask = make_mask(primary_data, primary_file, mask_df)
+        plume_mask = make_mask(primary_data, primary_time, mask_df)
 
         # get the individual plumes
         plume_positions = label_plumes(plume_mask)
 
         # visualise
-        make_plot(primary_data, plume_positions)
+        make_plot(visrad, primary_data, plume_positions)
 
 
 
