694d7f8c56fec140a07a86188637ead0cdef27bd
dabillox <danielfisher0@gmail.com>
2017-06-19 18:18:09 +0100

    fleshed out some code for extracting roi

--- src/features/resampling.py
+++ src/features/resampling.py
@@ -3,10 +3,12 @@ Resamples satellite data from one geographic
 projection to another.
 '''
 
-import pyresample
+import pyresample as pr
+from matplotlib.path import Path
+import numpy as np
 
 
-def roi_bounds(roi):
+def get_roi_bounds(roi):
     min_x = 99999
     max_x = 0
     min_y = 99999
@@ -20,11 +22,39 @@ def roi_bounds(roi):
             max_y = y
         if y < min_y:
             min_y = y
-    return [max_x, min_x, max_y, min_y]
+    return {'max_x': max_x,
+            'min_x': min_x,
+            'max_y': max_y,
+            'min_y': min_y}
 
+
+def get_mask(roi, rb):
+    extent = [[y - rb['min_y'], x - rb['min_x']] for y, x in roi.extent]
+
+    nx = rb['max_x'] - rb['min_x']
+    ny = rb['max_y'] - rb['min_y']
+
+    x, y = np.meshgrid(np.arange(nx), np.arange(ny))
+    x, y = x.flatten(), y.flatten()
+    points = np.vstack((x, y)).T
+
+    poly_verts = roi['extent']
+
+    # apply mask
+    path = Path(poly_verts)
+    grid = path.contains_points(points)
+    grid = grid.reshape((ny, nx))
+
+    return grid
 
 
 def resampler(orac_data, roi):
+
+    # first rename column names so that they are standardised
+    if 'plume_extent' in roi:
+        roi = roi.rename(columns={'plume_extent': 'extent'})
+    elif 'bg_extent' in roi:
+        roi = roi.rename(columns={'bg_extent': 'extent'})
 
     # extract roi and geographic grids.  So the algorithm to do this
     # is as follows: find the bounding coordinates of the roi, extract
@@ -33,6 +63,14 @@ def resampler(orac_data, roi):
     # min_x from all x coords, and min_y from all y coords).  Now draw the mask
     # in the bounding box using these normalised coordinates.  So with this, we
     # will have extracted the data for the subset and drawn a mask around it.
+    rb = get_roi_bounds(roi)
+    lat = orac_data['lat'][rb['min_y']:rb['max_y'],
+                           rb['min_x']:rb['max_x']]
+    lon = orac_data['lon'][rb['min_y']:rb['max_y'],
+                           rb['min_x']:rb['max_x']]
+    aod = orac_data['aod'][rb['min_y']:rb['max_y'],
+                           rb['min_x']:rb['max_x']]
+    mask = get_mask(roi, rb)
 
 
     # build resampling grid
