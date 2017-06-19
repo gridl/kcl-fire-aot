480cd595f0a5be192bc23f427d579d7845c9025e
Daniel Fisher <danielfisher0@gmail.com>
2017-06-19 14:26:22 +0100

    few more changes, and actually now reading in ORAC proced data correctly, not modis

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 
 import src.data.readers as readers
+import config
 
 
 def lc_subset():
@@ -47,37 +48,27 @@ def get_orac_fname(orac_file_path, plume):
     return glob.glob(os.path.join(orac_file_path, y, doy, 'main', '*' + time + '*.primary.nc'))[0]
 
 
-
 def main():
-
-
-    # set up filepaths and similar
-    root = '/Users/dnf/projects/kcl-fire-aot/data/'
-
-    orac_file_path = root + 'processed/orac_proc/'
-    goes_frp_file_path = root + 'processed/goes_frp/goes13_2014_fire_frp_atm.csv'
-    plume_mask_file_path = root + 'processed/plume_masks/myd021km_plumes_df.pickle'
-    lc_file_path = root + 'external/land_cover/GLOBCOVER_L4_200901_200912_V2.3.tif'
 
     # create df to hold the outputs
     output_df = pd.DataFrame()
 
     # read in non-plume specific files
-    frp_data = readers.read_goes_frp(goes_frp_file_path)
-    lc_data = readers.read_lc(lc_file_path)
+    frp_data = readers.read_goes_frp(config.goes_frp_file_path)
+    lc_data = readers.read_lc(config.lc_file_path)
 
     # iterate over each plume in the plume mask dataframe
     modis_filename = ''
-    plumes_masks = readers.read_plume_masks(plume_mask_file_path)
+    plumes_masks = readers.read_plume_masks(config.plume_mask_file_path)
     for index, plume in plumes_masks.iterrows():
 
         # if the plumes is from a different modis file, then
         # load in the correct ORAC processed file
         if plume.filename != modis_filename:
+            modis_filename = plume.filename
             try:
-                orac_filename = get_orac_fname(orac_file_path, plume)
+                orac_filename = get_orac_fname(config.orac_file_path, plume)
                 orac_data = readers.read_orac(orac_filename)
-                modis_filename = plume.filename
             except Exception, e:
                 print e
                 continue
