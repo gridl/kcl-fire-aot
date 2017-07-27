00926f2a19e0fdb062a3eec883baed0de29db7e5
dabillox <danielfisher0@gmail.com>
2017-07-27 15:23:37 +0100

    first go at multiple class plotting of plumes

--- src/visualization/display_plumes.py
+++ src/visualization/display_plumes.py
@@ -27,14 +27,14 @@ def get_primary_times(primary_files):
     times = [datetime.strptime(pf.split('_')[-2], '%Y%m%d%H%M').timetuple() for pf in primary_files]
     unique_times = list(set(times))
 
-    primary_datestring = []
+    primary_datestrings = []
     for tt in unique_times:
-        primary_datestring.append(str(tt.tm_year) + \
+        primary_datestrings.append(str(tt.tm_year) + \
                                   str(tt.tm_yday).zfill(3) + \
                                   '.' + \
                                   str(tt.tm_hour).zfill(2) + \
                                   str(tt.tm_min).zfill(2))
-    return primary_datestring
+    return unique_times, primary_datestrings
 
 
 def get_sub_df(primary_time, mask_df):
@@ -124,39 +124,34 @@ def label_plumes(mask):
     return plume_positions
 
 
-def make_plume_plot(fname, tcc, primary_data, plume_positions, plume_mask):
+def make_plume_plot(tcc, primary_data, pp, plume_mask, axis):
 
-    # iterate over the plumes
-    for i, pp in enumerate(plume_positions):
 
-        fig, axes = plt.subplots(1, 4, figsize=(20, 6))
+    for ax, k in zip(axis.flatten(), ['', 'cer', 'cot', 'costjm']):
 
-        for ax, k in zip(axes.flatten(), ['', 'cer', 'cot', 'costjm']):
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
+        # get the mask
+        mask = plume_mask[pp[0]:pp[1], pp[2]:pp[3]].astype('bool')
 
-            # get the mask
-            mask = plume_mask[pp[0]:pp[1], pp[2]:pp[3]].astype('bool')
+        if not k:
+            p = ax.imshow(tcc[pp[0]:pp[1], pp[2]:pp[3]], interpolation='None')
+            cbar = plt.colorbar(p, ax=ax)
+            cbar.ax.get_yaxis().labelpad = 15
+            cbar.ax.set_ylabel('dn', rotation=270, fontsize=14)
 
-            if not k:
-                p = ax.imshow(tcc[pp[0]:pp[1], pp[2]:pp[3]], interpolation='None')
-                cbar = plt.colorbar(p, ax=ax)
-                cbar.ax.get_yaxis().labelpad = 15
-                cbar.ax.set_ylabel('dn', rotation=270, fontsize=14)
-
+        else:
+            data = primary_data.variables[k][pp[0]:pp[1], pp[2]:pp[3]]
+            masked_data = np.ma.masked_array(data, mask=~mask)
+            if k == 'costjm':
+                p = ax.imshow(masked_data, interpolation='none', norm=LogNorm(vmin=1, vmax=1000))
             else:
-                data = primary_data.variables[k][pp[0]:pp[1], pp[2]:pp[3]]
-                masked_data = np.ma.masked_array(data, mask=~mask)
-                if k == 'costjm':
-                    p = ax.imshow(masked_data, interpolation='none', norm=LogNorm(vmin=1, vmax=1000))
-                else:
-                    p = ax.imshow(masked_data, interpolation='none', norm=LogNorm(vmin=0.01, vmax=1))
-                cbar = plt.colorbar(p, ax=ax)
-                cbar.ax.get_yaxis().labelpad = 15
-                cbar.ax.set_ylabel(k, rotation=270, fontsize=14)
-        plt.savefig(fname + '_p' + str(i) + '.png', bbox_inches='tight')
-    plt.close('all')
+                p = ax.imshow(masked_data, interpolation='none', norm=LogNorm(vmin=0.01, vmax=1))
+            cbar = plt.colorbar(p, ax=ax)
+            cbar.ax.get_yaxis().labelpad = 15
+            cbar.ax.set_ylabel(k, rotation=270, fontsize=14)
+
 
 
 def main():
@@ -173,41 +168,49 @@ def main():
     mask_df = pd.read_pickle(mask_path)
 
     # get the list of file times
-    primary_times = get_primary_times(glob.glob(orac_data_path + '*/*/*' + '.primary*'))
+    orac_times, l1b_times = get_primary_times(glob.glob(orac_data_path + '*/*/*' + '.primary*'))
 
 
-        with open(output_txt + "plume_extents.txt", "w") as text_file:
+    with open(output_txt + "plume_extents.txt", "w") as text_file:
 
+        for orac_time, l1b_time in zip(orac_times, l1b_times):
+
+            # get the tcc of the associated l1b file
+            l1b_file = glob.glob(l1b_path + '/*/*' + l1b_time + '*')[0]
+            tcc = tcc_myd021km(l1b_file)
+            fname = output + l1b_file.split('/')[-1][:-3] + '_orac_quicklooks'
+
+            # open up the orac products
+            prod_dict = {}
             for lut_class in lut_classes:
+                orac_file = glob.glob(orac_data_path + '*/*/*' + orac_time + '*' + lut_class + '*.primary*')
+                print orac_file
+                orac_data = open_primary(orac_file)
+                prod_dict[lut_class] = orac_data
 
-                # iterate over modis files
-                for primary_file in glob.glob(orac_data_path + '*/*/*' + lut_class + '.primary*'):
+            # get the plume mask for the scene
+            plume_mask = make_mask(orac_data, l1b_time, mask_df)
+            plume_positions = label_plumes(plume_mask)
 
-                    # first get the primary file time
-                    primary_time =
-                    fname = output + primary_file.split('/')[-1][:-3] + '_quicklook'
+            # iterate over plumes in scene
+            for i, pp in enumerate(plume_positions):
+                fig, axes = plt.subplots(len(lut_classes), 4, figsize=(20, 5*len(lut_classes)))
 
-                    # get the tcc of the associated l1b file
-                    l1b_file = glob.glob(l1b_path + '/*/*' + primary_time + '*')[0]
-                    tcc = tcc_myd021km(l1b_file)
-
-                    # open up the ORAC primary file
-                    primary_data = open_primary(primary_file)
-
-                    # get the plumes
-                    plume_mask = make_mask(primary_data, primary_time, mask_df)
-                    plume_positions = label_plumes(plume_mask)
+                # iterate over the classes
+                for lut_class in lut_classes:
 
                     # visualise
-                    make_plume_plot(fname, tcc, primary_data, plume_positions, plume_mask)
+                    make_plume_plot(tcc, prod_dict[lut_class], pp, plume_mask, axes[i])
 
-                # let dump coords of plume to text file for Caroline
-                for pp in plume_positions:
-                    text_file.write(primary_file.split('/')[-1] + " " +
-                                    str(pp[0]) + " " +
-                                    str(pp[1]) + " " +
-                                    str(pp[2]) + " " +
-                                    str(pp[3]) + "\n")
+                plt.savefig(fname + '_p' + str(i) + '.png', bbox_inches='tight')
+                plt.close('all')
+
+                # dump plume pos for caroline
+                text_file.write(orac_file.split('/')[-1] + " " +
+                                str(pp[0]) + " " +
+                                str(pp[1]) + " " +
+                                str(pp[2]) + " " +
+                                str(pp[3]) + "\n")
 
 if __name__ == '__main__':
     main()
