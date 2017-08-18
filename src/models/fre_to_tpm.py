77b142250c48eeeb123854cf5a5806958b4ff38d
Daniel Fisher <danielfisher0@gmail.com>
2017-08-18 17:40:45 +0100

    end of day commit.  Still need to get landcover read in properly

--- src/models/fre_to_tpm.py
+++ src/models/fre_to_tpm.py
@@ -19,53 +19,62 @@ Basic logic:
 
 import ast
 import logging
+import glob
+import os
 
 import pandas as pd
 import numpy as np
 import pyresample as pr
+from netCDF4 import Dataset
+from pyhdf.SD import SD, SDC
+from matplotlib.path import Path
+from scipy import stats
 
-import src.data.readers as readers
+
 import src.config.filepaths as filepaths
 
 
-# TODO include subsetting by latitudes in this functiom.
-def resample_landcover(lc_data, sub_lats, sub_lons):
+def get_orac_fname(orac_file_path, plume):
+    y = plume.filename[10:14]
+    doy = plume.filename[14:17]
+    time = plume.filename[18:22]
+    return glob.glob(os.path.join(orac_file_path, y, doy, 'main', '*' + time + '*.primary.nc'))[0]
 
-    # Dealing with dateline crossings.
-    if (np.min(sub_lons) - -180) < 0.01:
-        first_line_max_lon = np.max(sub_lons[0,:])
-        min_max_lon = np.min(sub_lons[sub_lons > first_line_max_lon]) # the min of the longitudes that cross dateline
+def find_landcover_class(plume, landcover):
 
-        lon_index_west = int((first_line_max_lon - (-180)) / 360 * lc_data['lon'].size)  # lon index
-        lon_index_east = int((min_max_lon - (-180)) / 360 * lc_data['lon'].size)  # lon index
-        lat_index_north = int((np.max(sub_lats) - 90) * -1 / 180 * lc_data['lat'].size) # lat index
-        lat_index_south = int((np.min(sub_lats) - 90) * -1 / 180 * lc_data['lat'].size)  # lat index
+    y = plume.filename[10:14]
+    doy = plume.filename[14:17]
+    time = plume.filename[18:22]
 
-        # we can run from 0 up to lon_index
-        water_mask_west = np.array(lc_data['wb_class'][lat_index_north:lat_index_south, 0:lon_index_west])
-        water_mask_east = np.array(lc_data['wb_class'][lat_index_north:lat_index_south, lon_index_east:])
+    myd14 = glob.glob(os.path.join(filepaths.path_to_modis_frp, '*A' + y + doy + '.' + time + '*.hdf'))[0]
+    myd14 = SD(myd14, SDC.READ)
+    
+    lines = myd14.select('FP_line').get()
+    samples = myd14.select('FP_sample').get()
+    lats = myd14.select('FP_latitude').get()
+    lons = myd14.select('FP_longitude').get()
 
-        # now lets join the masks
-        water_mask = np.concatenate((water_mask_east, water_mask_west), axis=1)
+    poly_verts = plume['plume_extent']
+    bb_path = Path(poly_verts)
 
-        # build the geo grids for the masks
-        lons_west = np.tile(lc_data['lon'][0:lon_index_west], (water_mask_west.shape[0], 1))
-        lons_east = np.tile(lc_data['lon'][lon_index_east:], (water_mask_east.shape[0], 1))
-        lons = np.concatenate((lons_east, lons_west), axis=1)
-        lats = np.transpose(np.tile(lc_data['lat'][lat_index_north:lat_index_south],
-                                    (water_mask_east.shape[1] + water_mask_west.shape[1], 1)))
+    # find the geographic coordinates of fires inside the plume mask
+    lat_list = []
+    lon_list = []
+    for l, s, lat, lon in zip(lines, samples, lats, lons):
+        if bb_path.contains_point((s, l)):
+            lat_list.append(lat)
+            lon_list.append(lon)
 
-    else:
-        lon_index_west = int((np.min(sub_lons) - (-180)) / 360 * lc_data['lon'].size)  # lon index
-        lon_index_east = int((np.max(sub_lons) - (-180)) / 360 * lc_data['lon'].size)  # lon index
-        lat_index_north = int((np.max(sub_lats) - 90) * -1 / 180 * lc_data['lat'].size)  # lat index
-        lat_index_south = int((np.min(sub_lats) - 90) * -1 / 180 * lc_data['lat'].size)  # lat index
+    # now get the landcover points
+    lc_list = []
+    for lat, lon in zip(lat_list, lon_list):
+        s = int((lon - (-180)) / 360 * landcover['lon'].size)  # lon index
+        l = int((lat - 90) * -1 / 180 * landcover['lat'].size)  # lat index
+        lc_list.append(np.array(landcover['Band1'][s:s+1, l:l+1][0]))
 
-        water_mask = np.array(lc_data['wb_class'][lat_index_north:lat_index_south, lon_index_west:lon_index_east])
-        lons = np.tile(lc_data['lon'][lon_index_west:lon_index_east], (water_mask.shape[0], 1))
-        lats = np.transpose(np.tile(lc_data['lat'][lat_index_north:lat_index_south], (water_mask.shape[1], 1)))
+    # return the most common landcover class for the fire contined in the ROI
+    return stats.mode(lc_list)
 
-    return water_mask, lons, lats
 
 
 def main():
@@ -75,10 +84,24 @@ def main():
     landcover_path = root_path + 'Global/land_cover/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7_900m.nc'
     mask_path = root_path + 'Americas/processed/plume_masks/myd021km_plumes_df.pickle'
 
+    # open up the landcover dataset
+    landcover = Dataset(landcover_path)
+
     try:
         mask_df = pd.read_pickle(mask_path)
     except:
         mask_df = pd.read_csv(mask_path, quotechar='"', sep=',', converters={'plume_extent': ast.literal_eval})
+
+    for index, plume in mask_df.iterrows():
+
+        # find landcover type
+        fire_lc_class = find_landcover_class(plume, landcover)
+
+        # find aod / tpm
+
+        # find fre
+
+
 
 
 if __name__ == "__main__":
