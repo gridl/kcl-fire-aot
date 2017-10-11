d93bc2bfb491c140dfc7b800739ca55bc706cdbb
dabillox <danielfisher0@gmail.com>
2017-10-11 10:57:27 +0100

    conded up tpm calculator

--- src/models/fre_to_tpm/ftt_processor.py
+++ src/models/fre_to_tpm/ftt_processor.py
@@ -85,7 +85,7 @@ def main():
         # get the variables of interest
         # if start_time is not None:
         #     fre.append(ft.compute_fre(plume_polygon, frp_df, start_time, stop_time, utm_resampler))
-        tpm.append(tt.compute_aod(orac_aod, myd04_aod, plume_bounding_box, plume_mask, plume_lats, plume_lons))
+        tpm.append(tt.compute_tpm(orac_aod, myd04_aod, plume_bounding_box, plume_mask, plume_lats, plume_lons))
         lc.append(0)
 
     # split data based on lc type
--- src/models/fre_to_tpm/ftt_tpm.py
+++ src/models/fre_to_tpm/ftt_tpm.py
@@ -1,46 +1,31 @@
-
-# TODO still need to make sure area is being calculated correctly
-def compute_plume_area(utm_plume_polygon):
-    # # TODO is sinusoidal proj good enough?  Yes it is: https://goo.gl/KE3tuY
-    # # get extra accuracy by selecting an appropriate lon_0
-    # m = Basemap(projection='sinu', lon_0=140, resolution='c')
-    #
-    # lons = (lons + 180) - np.floor((lons + 180) / 360) * 360 - 180;
-    # zone = stats.mode(np.floor((lons + 180) / 6) + 1, axis=None)[0][0]
-    # p = pyproj.Proj(proj='utm', zone=zone, ellps='WGS84', datum='WGS84')
-    #
-    # # apply to shapely polygon
-    # projected_plume_polygon_m = transform(m, plume_polygon)
-    # projected_plume_polygon_p = transform(p, plume_polygon)
-
-    # compute projected polygon area in m2
-    # return projected_plume_polygon_m.area, projected_plume_polygon_p.area
-
-    # we already have the plume polygon area
-    return utm_plume_polygon.area
+import numpy as np
 
 
-def compute_aod(plume_bounding_pixels, plume_mask, lats, lons):
-    '''
-    Resampling of the ORAC AOD data is required to remove the bowtie effect from the data.  We can then
-    sum over the AODs contained with the plume.
+def extract_mean_aod(aod, mask):
+    return np.mean(aod[mask])
 
-    Resampling of the MODIS AOD data is required so that it is in the same projection as the ORAC AOD.
-    With it being in the same projection we can replace low quality ORAC AODs with those from MXD04 products.
-    We also need to get the background AOD data from MXD04 products as ORAC does not do optically thin
-    retrievals well.
 
-    Also, we need to check that the pixel area estimates being computed by compute_plume_area are reasonable.
-    That can be done in this function, we just produce another area estimate from the resampled mask by getting
-    vertices, getting the lat lons of the vertices, creating a shapely polygon from them and then computing the area.
-    '''
+def compute_tpm(orac_aod, myd04_aod, plume_polygon, plume_mask, bg_mask):
 
-    # create best AOD map from ORAC and MYD04 AOD
+    pm_factor = 4.3  # TODO add this to a config file
 
-    # extract best AOD using plume mask
+    # get plume area
+    plume_area = plume_polygon.area
 
-    # split into background and plume AODs
+    # extract mean ORAC plume AOD using plume mask
+    mean_plume_aod = extract_mean_aod(orac_aod, plume_polygon)
 
-    # subtract background from plume
+    # extract mean MYD04 background AOD using bg_mask
+    mean_bg_aod = extract_mean_aod(myd04_aod, plume_polygon)
 
-    # return plume AOD
+    # subtract mean background AOD from mean plume AOD
+    mean_plume_aod -= mean_bg_aod
+
+    # convert to PM using conversion factor
+    plume_pm = mean_plume_aod*pm_factor
+
+    # multiply by plume area to get total tpm
+    tpm = plume_pm * plume_area
+
+    # return plume TPM
+    return tpm
