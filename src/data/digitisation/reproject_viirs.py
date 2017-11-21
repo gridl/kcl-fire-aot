e3a16a1b65431bbb8bb16f73ae625153b7f32a60
dabillox <danielfisher0@gmail.com>
2017-11-21 18:41:33 +0000

    written viirs reprojection code

---
+++ src/data/digitisation/reproject_viirs.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import glob
+import re
+
+import h5py
+import numpy as np
+import scipy.misc as misc
+
+import src.config.filepaths as fp
+import src.features.fre_to_tpm.ftt_utils as ut
+
+
+def get_timestamp(viirs_sdr_fname):
+    try:
+        return re.search("[d][0-9]{8}[_][t][0-9]{6}", viirs_sdr_fname).group()
+    except Exception, e:
+        logger.warning("Could not extract time stamp from: " + viirs_sdr_fname + " with error: " + str(e))
+        return ''
+
+
+def get_viirs_fname(path, timestamp_viirs, viirs_sdr_fname):
+    fname = [f for f in os.listdir(path) if timestamp_viirs in f]
+    if len(fname) > 1:
+        logger.warning("More that one frp granule matched " + viirs_sdr_fname + "selecting 0th option")
+        return fname[0]
+    elif len(fname) == 1:
+        return fname[0]
+    else:
+        return ''
+
+
+def read_h5(f):
+    return h5py.File(f,  "r")
+
+
+def create_resampler(viirs_data):
+    lats = viirs_data['All_Data']['VIIRS-MOD-GEO_All']['Latitude'][:]
+    lons = viirs_data['All_Data']['VIIRS-MOD-GEO_All']['Longitude'][:]
+    return ut.utm_resampler(lats, lons, 750)
+
+
+def image_histogram_equalization(image, number_bins=256):
+    # from http://www.janeriksolem.net/2009/06/histogram-equalization-with-python-and.html
+
+    # get image histogram
+    image_histogram, bins = np.histogram(image[image > 0].flatten(), number_bins, normed=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+    return image_equalized.reshape(image.shape)
+
+
+def tcc_viirs(viirs_data, resampler):
+    #m1_params = viirs_data['All_Data']['VIIRS-M1-SDR_All']['RadianceFactors']
+    m1 = viirs_data['All_Data']['VIIRS-M1-SDR_All']['Radiance'][:]
+    #m4_params = viirs_data['All_Data']['VIIRS-M1-SDR_All']['RadianceFactors']
+    m4 = viirs_data['All_Data']['VIIRS-M4-SDR_All']['Radiance'][:]
+    #m5_params = viirs_data['All_Data']['VIIRS-M1-SDR_All']['RadianceFactors']
+    m5 = viirs_data['All_Data']['VIIRS-M5-SDR_All']['Radiance'][:]
+
+    mask = m5<0
+    masked_lats = np.ma.masked_array(resampler.lats, mask)
+    masked_lons = np.ma.masked_array(resampler.lons, mask)
+
+    resampled_m1 = resampler.resample_image(m1, masked_lats, masked_lons, fill_value=0)
+    resampled_m4 = resampler.resample_image(m4, masked_lats, masked_lons, fill_value=0)
+    resampled_m5 = resampler.resample_image(m5, masked_lats, masked_lons, fill_value=0)
+
+    r = image_histogram_equalization(resampled_m5)
+    g = image_histogram_equalization(resampled_m4)
+    b = image_histogram_equalization(resampled_m1)
+
+    r = np.round((r * (255 / np.max(r))) * 1).astype('uint8')
+    g = np.round((g * (255 / np.max(g))) * 1).astype('uint8')
+    b = np.round((b * (255 / np.max(b))) * 1).astype('uint8')
+
+    rgb = np.dstack((r, g, b))
+    return rgb
+
+
+def extract_aod(viirs_aod, resampler):
+    aod = viirs_aod['All_Data']['VIIRS-Aeros-Opt-Thick-IP_All']['faot550'][:]
+    mask = aod < -1
+    masked_lats = np.ma.masked_array(resampler.lats, mask)
+    masked_lons = np.ma.masked_array(resampler.lons, mask)
+    resampled_aod = resampler.resample_image(aod, masked_lats, masked_lons, fill_value=0)
+
+    return resampled_aod
+
+
+
+
+def main():
+
+
+    for viirs_sdr_fname in os.listdir(fp.path_to_viirs_sdr_unproj):
+
+        logger.info("Processing viirs file: " + viirs_sdr_fname)
+
+        if 'DS' in viirs_sdr_fname:
+            continue
+
+        timestamp_viirs = get_timestamp(viirs_sdr_fname)
+        if not timestamp_viirs:
+            continue
+
+        try:
+            aod_fname = get_viirs_fname(fp.path_to_viirs_aod_unproj, timestamp_viirs, viirs_sdr_fname)
+        except Exception, e:
+            logger.warning('Could not load aux file for:' + viirs_sdr_fname + '. Failed with ' + str(e))
+            continue
+
+        try:
+            viirs_sdr = read_h5(os.path.join(fp.path_to_viirs_sdr_unproj, viirs_sdr_fname))
+
+            # setup resampler adn extract true colour
+            utm_resampler = create_resampler(viirs_sdr)
+            tcc = tcc_viirs(viirs_sdr, utm_resampler)
+
+        except Exception, e:
+            logger.warning('Could not read the input file: ' + viirs_sdr_fname + '. Failed with ' + str(e))
+            continue
+
+        # if filenames load in data
+        viirs_aod = None
+        if aod_fname:
+            try:
+                viirs_aod_data = read_h5(os.path.join(fp.path_to_viirs_aod_unproj, aod_fname))
+                viirs_aod = extract_aod(viirs_aod_data, utm_resampler)
+            except Exception, e:
+                logger.warning('Could not read aod file: ' + aod_fname)
+        if viirs_aod is None:
+            continue
+
+
+        # save the outputs
+        misc.imsave(os.path.join(fp.path_to_viirs_aod_resampled, aod_fname.replace('h5', 'png')), viirs_aod)
+        misc.imsave(os.path.join(fp.path_to_viirs_sdr_resampled, viirs_sdr_fname.replace('h5', 'png')), tcc)
+
+
+if __name__ == "__main__":
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger(__name__)
+    main()
