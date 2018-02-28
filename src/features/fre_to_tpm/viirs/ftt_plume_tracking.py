fixed plume intersection issue for EW blowing plumes.  Added in the ORAC AOD background calculation for optically thick background.

src/features/fre_to_tpm/viirs/ftt_plume_tracking.py
@@ -303,11 +303,11 @@ def angle_between(v1, v2):
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 
-def assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector, pix_size=1000):
+def assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector, resampled_pix_size):
     """
 
     :param current_vector: the current flow vector for checking angle of flow vectors
-    :param flow: the current flow vectors from the feature tracking
+    :param flow: the current flowfc vectors from the feature tracking
     :param flow_means: the vector containing the flow means
     :param flow_sds: the vector containing the flow standard deviations
     :param flow_nobs: the vector containing the number of flow observations
@@ -337,8 +337,8 @@ def assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, c
         # here is where the flow update occurs
         flow = flow[flow_mask]
 
-        flow_means[i, :] = np.mean(flow, axis=0) * pix_size
-        flow_sds[i, :] = np.std(flow, axis=0) * pix_size
+        flow_means[i, :] = np.mean(flow, axis=0) * resampled_pix_size
+        flow_sds[i, :] = np.std(flow, axis=0) * resampled_pix_size
         flow_nobs[i] = len(tracks)
 
     # check if any flows of zero and update with most recent flow estimate
@@ -412,6 +412,7 @@ def find_integration_start_stop_times(p_number,
                                       utm_resampler,
                                       timestamp,
                                       frp_df,
+                                      resampled_pix_size,
                                       plot=True):
     """
     Main function to compute the time stamps over which we need to integrate the
@@ -463,7 +464,7 @@ def find_integration_start_stop_times(p_number,
     if plot:
         fires = []
 
-    stopping_thresh = 1000  # stopping condition in metres TODO move to config
+    stopping_thresh = resampled_pix_size  # stopping condition in metres TODO move to config
 
     # set time variables that will be returned
     t1 = None
@@ -509,7 +510,7 @@ def find_integration_start_stop_times(p_number,
         tracks, flow = compute_flow(tracks, f2_subset_reproj, f1_subset_reproj)
 
         # compute mean flow for plume and update current flow vector
-        assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector)
+        assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector, resampled_pix_size)
         if (flow_means[i] != 0).any():
             current_vector = flow_means[i, :]
 
@@ -557,7 +558,7 @@ def find_integration_start_stop_times(p_number,
 def find_flow(p_number, plume_logging_path, utm_plume_points,
               utm_plume_vect, plume_lats, plume_lons,
               geostationary_lats, geostationary_lons,
-              timestamp, utm_resampler_plume, frp_df, plot=False):
+              timestamp, utm_resampler_plume, frp_df, resampled_pix_size, plot=False):
 
     # get the utm plume vector and compute its length.  This will
     # be used to check when we hae reached the full length of the plume
@@ -573,6 +574,7 @@ def find_flow(p_number, plume_logging_path, utm_plume_points,
         geo_polygon = Polygon(zip(bounding_lons, bounding_lats))
         utm_geo_polygon = ut.reproject_shapely(geo_polygon, utm_resampler_plume)
         fires = []
+
     geostationary_lats_subset, geostationary_lons_subset = subset_geograpic_data(geostationary_lats, geostationary_lons,
                                                                                  bb)
 
@@ -592,14 +594,15 @@ def find_flow(p_number, plume_logging_path, utm_plume_points,
     flow_means, flow_sds, projected_flow_means = np.zeros([72, 2]), np.zeros([72, 2]), np.zeros([72, 2])
     flow_nobs, flow_update, projected_flow_magnitude = np.zeros([72]), np.zeros([72]), np.zeros(72)
     tracks = []
-    stopping_thresh = 1000  # stopping condition in metres TODO move to config
+    stopping_thresh = resampled_pix_size  # stopping when within one pix
     current_vector = vector.copy()
 
     # iterate over geostationary files
     for i in xrange(len(geostationary_fnames)-1):
 
         # setup imagery for tracking, only reproject both images if we are on the
-        # first iteration, else just reassign and reproject most recent.
+        # first iteration, else just reassign and reproject most recent.  They are
+        # reprojected to the plume so will have the same resolution as the plume resample
         if i == 0:
             f1_subset, f1_display_subset = extract_observation(geostationary_fnames[i], bb, min_image_segment)
             f2_subset, f2_display_subset = extract_observation(geostationary_fnames[i + 1], bb, min_image_segment)
@@ -636,7 +639,7 @@ def find_flow(p_number, plume_logging_path, utm_plume_points,
         tracks, flow = compute_flow(tracks, f2_subset_reproj, f1_subset_reproj)
 
         # compute robust mean flow for plume and update current flow vector
-        assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector)
+        assess_flow(flow, flow_means, flow_sds, flow_nobs, flow_update, tracks, i, current_vector, resampled_pix_size)
         if (flow_means[i] != 0).any():
             current_vector = flow_means[i, :]
 
@@ -664,7 +667,6 @@ def find_flow(p_number, plume_logging_path, utm_plume_points,
         vis.run_plot(plot_images, fires, flow_means, projected_flow_means,
                      plume_head, plume_tail, utm_plume_points, utm_resampler_plume,
                      plume_logging_path, fnames, i)
-
 
     # return the projected flow means in UTM coords, and the list of himawari filenames asspocated with the flows
     return projected_flow_means[:i+1], geostationary_fnames[:i+1]

src/features/fre_to_tpm/viirs/ftt_processor.py
@@ -22,20 +22,68 @@ logger = logging.getLogger(__name__)
 
 # TODO make some dictionaires to hold all the data and get them out of main
 
+'''
+Still Todo:
+3. Get all fires near to the head of the plume (Say within some radius i.e. 10km).
+
+Algorithm description
+
+Load in data
+Iterate over each hand digitised plume
+    IF current timestamp is different
+        Load in all AOD data
+        Construct UTM resampler for image at 750m resolution from ORAC AOD
+        Build VIIRS no data mask from ORAC AOD
+        Mask out all VIIRS deletions (ORAC AOD, VIIRS AOD, lats, lons, flags, costs)
+    Build bounding box for hand digitsed plume (rectangular box based on (min,max) pos + padding)
+    Get geographic data for plume bounding box
+    Get the lat/lon for the head and tail of the plume direction vector
+    Get the lats/lons for the points of the plume polygon
+    Build binary plume mask for bounding box
+
+    Build a shapely LINESTRING object for plume vector
+    Build a shapely MULTIPOINT object based on plume polygon
+    Build a shape POLYGON object based on plume polygon
+
+    Build bounding box for hand digisited background extent
+    Build binary mask for the background
+
+    Subset all data (AOD, FLAG, COST) to either plume or background bounding boxes
+
+    Construct UTM resampler for plume at 750m resolution from plume lat/lon grid
+    Reproject SHAPELY objects to UTM using plume resampler
+
+    Compute optical flow (see below for approach)
+    Based on mean flow at each time step segemnt the XXX polygon
+
+
+
+
+
+
+
+
+
+'''
+
 
 def main():
 
     plot = True
 
+    # TODO add to config (it is pixe size that resample to)
+    resampled_pix_size = 750
+
     # load in static data
     frp_df = ut.read_frp_df(fp.path_to_himawari_frp)
+    #frp_df = None
 
     plume_df = ut.read_plume_polygons(fp.path_to_smoke_plume_polygons_viirs_csv)
     geo_file = fp.root_path + '/processed/himawari/Himawari_lat_lon.img'
     geostationary_lats, geostationary_lons = load_hrit.geo_read(geo_file)
 
     # setup output path to hold csv
-    output_path = os.path.join(fp.path_to_frp_tpm_features, 'model_features_viirs.csv')
+    output_path = os.path.join(fp.path_to_frp_tpm_features, 'model_features_viirs_OAODlt1.csv')
 
     # set timestamp to check if new data loaded in
     previous_timestamp = ''
@@ -46,7 +94,7 @@ def main():
     # itereate over the plumes
     for p_number, plume in plume_df.iterrows():
 
-        #if p_number != 13: continue
+        if p_number != 21: continue
 
         # make a directory to hold the plume logging information
         plume_logging_path = os.path.join(fp.path_to_plume_tracking_visualisations_viirs, str(p_number))
@@ -56,7 +104,8 @@ def main():
         # get plume time stamp
         current_timestamp = ut.get_timestamp(plume.filename)
 
-        # if working on a new scene. Then set it up
+        # if working on a new scene. Then set it up by applying no
+        # data masks and resampling everything to UTM grid at 750m
         if current_timestamp != previous_timestamp:
 
             try:
@@ -70,7 +119,7 @@ def main():
             # set up resampler
             utm_image_resampler = ut.utm_resampler(orac_aod_data.variables['lat'][:],
                                              orac_aod_data.variables['lon'][:],
-                                             750)
+                                             resampled_pix_size)
 
             # get the mask for the lats and lons and apply
             orac_aod = ut.orac_aod(orac_aod_data)
@@ -78,6 +127,7 @@ def main():
             masked_lats = np.ma.masked_array(utm_image_resampler.lats, mask)
             masked_lons = np.ma.masked_array(utm_image_resampler.lons, mask)
 
+            # resample all the datasets to UTM
             viirs_aod_utm = utm_image_resampler.resample_image(ut.viirs_aod(viirs_aod_data),
                                                          masked_lats, masked_lons, fill_value=0)
             viirs_flag_utm = utm_image_resampler.resample_image(ut.viirs_flags(viirs_aod_data),
@@ -99,18 +149,14 @@ def main():
             plume_lons = ut.subset_data(lons, plume_bounding_box)
 
             vector_lats, vector_lons = ut.extract_subset_geo_bounds(plume.plume_vector, plume_bounding_box,
-                                                                        plume_lats, plume_lons)
-
-
-
-            plume_vector = ut.construct_shapely_vector(vector_lats, vector_lons)
-
+                                                                    plume_lats, plume_lons)
             poly_lats, poly_lons = ut.extract_subset_geo_bounds(plume.plume_extent, plume_bounding_box,
                                                                         plume_lats, plume_lons)
+            plume_mask = ut.construct_mask(plume.plume_extent, plume_bounding_box)
+
+            plume_vector = ut.construct_shapely_vector(vector_lats, vector_lons)
             plume_points = ut.construct_shapely_points(poly_lats, poly_lons)
             plume_polygon = ut.construct_shapely_polygon(poly_lats, poly_lons)
-
-            plume_mask = ut.construct_mask(plume.plume_extent, plume_bounding_box)
 
             background_bounding_box = ut.construct_bounding_box(plume.background_extent)
             background_mask = ut.construct_mask(plume.background_extent, background_bounding_box)
@@ -121,7 +167,6 @@ def main():
             logger.error(str(e))
             continue
 
-
         # subset the data to the rois
         viirs_aod_utm_plume = ut.subset_data(viirs_aod_utm, plume_bounding_box)
         viirs_flag_utm_plume = ut.subset_data(viirs_flag_utm, plume_bounding_box)
@@ -130,9 +175,11 @@ def main():
 
         viirs_aod_utm_background = ut.subset_data(viirs_aod_utm, background_bounding_box)
         viirs_flag_utm_background = ut.subset_data(viirs_flag_utm, background_bounding_box)
+        orac_aod_utm_background = ut.subset_data(orac_aod_utm, background_bounding_box)
+        orac_cost_utm_background = ut.subset_data(orac_cost_utm, background_bounding_box)
 
-        # reproject vector data
-        utm_resampler_plume = ut.utm_resampler(plume_lats, plume_lons, 750)
+        # Reproject Shapely objects to UTM using plume geographic info
+        utm_resampler_plume = ut.utm_resampler(plume_lats, plume_lons, resampled_pix_size)
         utm_plume_points = ut.reproject_shapely(plume_points, utm_resampler_plume)
         utm_plume_polygon = ut.reproject_shapely(plume_polygon, utm_resampler_plume)
         utm_plume_vector = ut.reproject_shapely(plume_vector, utm_resampler_plume)
@@ -174,7 +221,7 @@ def main():
                                                                 utm_plume_vector, plume_lats, plume_lons,
                                                                 geostationary_lats, geostationary_lons,
                                                                 current_timestamp, utm_resampler_plume,
-                                                                frp_df, plot=plot)
+                                                                frp_df, resampled_pix_size, plot=plot)
 
             # the flow is computed back in time from the most recent plume extent to the oldest.
             # We need to work out how much of the oldest plume extent is attributable to the
@@ -214,16 +261,21 @@ def main():
                 sub_plume_polygon = ut.construct_shapely_polygon(bounding_lats, bounding_lons)
                 utm_sub_plume_polygon = ut.reproject_shapely(sub_plume_polygon, utm_resampler_plume)
 
-                # get intersection of plume and sub_plume polygons
+                # get intersection of plume and sub_plume polygons.  The reason for this is that
+                # the plume polygon has the shape of the plume, whilst the sub plume polygon has
+                # the shape of the bounding box (i.e. rectangular).  By taking the intersection
+                # we get the segment from the both the plume and the sub part of the boudning box.
+
                 try:
                     utm_sub_plume_polygon = utm_sub_plume_polygon.intersection(utm_plume_polygon)
                 except Exception, e:
                     logger.error(str(e))
                     continue
 
-
                 # get background aod for sub plume
-                bg_aod_dict = tt.extract_bg_aod(viirs_aod_utm_background, viirs_flag_utm_background, background_mask)
+                bg_aod_dict = tt.extract_bg_aod(viirs_aod_utm_background, viirs_flag_utm_background,
+                                                orac_aod_utm_background, orac_cost_utm_background,
+                                                background_mask)
 
                 # compute TPM
                 out_dict = tt.compute_tpm(viirs_aod_utm_plume, viirs_flag_utm_plume,

src/features/fre_to_tpm/viirs/ftt_tpm.py
@@ -54,11 +54,11 @@ def extract_best_mean_aod(d,
 
         plt.imshow(np.ma.masked_array(orac_cost, ~updated_plume_mask), vmax=10, cmap='plasma')
         plt.colorbar()
-        plt.savefig(os.path.join(sub_plume_logging_path, 'viirs_sub_plume_orac_cose.png'))
+        plt.savefig(os.path.join(sub_plume_logging_path, 'viirs_sub_plume_orac_cost.png'))
         plt.close()
 
         viirs_mask = updated_plume_mask & (viirs_flag <= 1)
-        orac_mask = updated_plume_mask & (viirs_flag > 1) & (orac_cost <= 3)
+        orac_mask = updated_plume_mask & (viirs_flag > 1) & (orac_cost <= 1)
         combined = np.zeros(viirs_mask.shape)
         combined[orac_mask] = orac_aod[orac_mask]
         combined[viirs_mask] = viirs_aod[viirs_mask]
@@ -76,7 +76,7 @@ def extract_best_mean_aod(d,
 
     # get the good data masks  # TODO Add to config file
     viirs_good = viirs_flag <= 1
-    orac_good = orac_cost <= 3
+    orac_good = orac_cost <= 1
     both_good = viirs_good & orac_good
     either_good = viirs_good | orac_good
     both_aod_gt_1_lt_2 = (viirs_aod >= 1) & (viirs_aod < 2) & (orac_aod >= 1) & (orac_aod < 2)
@@ -117,11 +117,29 @@ def extract_best_mean_aod(d,
     d['coverage_plume'] = np.sum(updated_plume_mask * 1.) / np.sum(plume_mask)
 
 
-def extract_bg_aod(viirs_aod, viirs_flag, mask):
-    viirs_aod = viirs_aod[mask]
-    viirs_flag = viirs_flag[mask]
-    return {'mean_bg_aod': np.mean(viirs_aod[viirs_flag == 0]),
-            'std_bg_aod': np.std(viirs_aod[viirs_flag == 0])}
+def extract_bg_aod(viirs_aod, viirs_flag, orac_aod, orac_cost, mask):
+    masked_viirs_aod = viirs_aod[mask]
+    masked_viirs_flag = viirs_flag[mask]
+
+    masked_orac_aod = orac_aod[mask]
+    masked_orac_cost = orac_cost[mask]
+
+    if np.min(viirs_flag[mask]) < 2:
+        aod_mean = np.mean(masked_viirs_aod[masked_viirs_flag == 0])
+        aod_std = np.std(masked_viirs_aod[masked_viirs_flag == 0])
+        typ = 'sp'
+    elif np.min(masked_orac_cost[mask] < 3):
+        aod_mean = np.mean(masked_orac_aod[masked_orac_cost < 3])
+        aod_std = np.std(masked_orac_aod[masked_orac_cost < 3])
+        typ = 'sp'
+    else:
+        aod_mean = np.nan
+        aod_std = np.nan
+        typ = 'failed'
+
+    return {'mean_bg_aod': aod_mean,
+            'std_bg_aod': aod_std,
+             'bg_type': typ}
 
 
 def compute_tpm(viirs_aod_utm_plume, viirs_flag_utm_plume,
@@ -135,6 +153,7 @@ def compute_tpm(viirs_aod_utm_plume, viirs_flag_utm_plume,
         # put background data into d
         d['mean_bg_aod'] = bg_aod_dict['mean_bg_aod']
         d['std_bg_aod'] = bg_aod_dict['std_bg_aod']
+        d['bg_type'] = bg_aod_dict['bg_type']
 
         d['pm_factor'] = 4.3  # m2/g TODO add this to a config file
 

src/features/fre_to_tpm/viirs/ftt_utils.py
@@ -346,15 +346,15 @@ def split_plume_polgons(plume_logging_path, bb,
                         plume_mask, plot=True):
 
     # compute orthogonal slope of plume vector in pixels
-    perp_slope = compute_perpendicular_slope(plume_vector)
+    m = compute_perpendicular_slope(plume_vector)
 
     y_shape, x_shape = plume_lats.shape
 
     # set up iterator variables
     a = [0, 0]  # ll
     b = [0, 0]  # ul
-    c = [0, x_shape-1]  # ur
-    d = [0, x_shape-1]  # lr
+    c = [0, 0]
+    d = [0, 0]
     tail_position = np.array(utm_plume_vector.coords[0])
 
     # set up list to hold polygon corners
@@ -374,16 +374,40 @@ def split_plume_polgons(plume_logging_path, bb,
         # convert UTM point to lat lon
         flow_lon, flow_lat = resampler.resample_point_to_geo(tail_position[1], tail_position[0])
 
-        # convert lat lon to pixel index
+        # convert lat lon to pixel index, to give us a point on the line
         dists = np.abs(flow_lat - plume_lats) + np.abs(flow_lon - plume_lons)
         sub_y, sub_x = divmod(dists.argmin(), x_shape)
 
-        # using slope compute y position at x min and x max
-        min_y = sub_y - perp_slope*sub_x   # b = y - mx, and when x=0, y=b
-        max_y = perp_slope*x_shape + min_y  # y at x = n
+        # using slope, and point on the point, get b so we know have full linear equation
+        slope = sub_y - m*sub_x   # b = y - mx, and when x=0, y=b
 
-        a[0] = min_y
-        d[0] = max_y
+        # using linear equation determine the intersections of the line with the x and y
+        # axes.  If the point of intersection does not exceed the extent of the ROI in the given axis then it is
+        # the direction colinear with the plume, and the segmentation must be performed in
+        # this axis.
+        y0 = m*0 + slope
+        y1 = m*x_shape + slope
+        x0 = (0-slope) / m
+        x1 = (y_shape-slope) / m
+
+        # check if we are updatin in the y direction, else assume x
+        update_y = (y0 > 0) & (y0 < y_shape) & (y1 > 0) & (y1 < y_shape)
+
+        if update_y:
+            # do y parts
+            a[0] = y0
+            d[0] = y1
+            # do x parts
+            c[1] = x_shape-1  # ur
+            d[1] = x_shape-1  # lr
+        else:
+            # do x parts
+            c[1] = x0
+            d[1] = x1
+            # do y parts
+            a[0] = y_shape - 1  # ur
+            d[0] = y_shape - 1  # lr
+
 
         # define mask
         polygon_corner_dict[i] = [a[:], b[:], c[:], d[:]]
@@ -394,12 +418,21 @@ def split_plume_polgons(plume_logging_path, bb,
             sub_y_positions.append(sub_y)
 
         # update polygon corner arrays
-        b = a[:]
-        c = d[:]
+        if update_y:
+            b = a[:]
+            c = d[:]
+        else:
+            b = c[:]
+            a = d[:]
 
     # now get the final part of the plume
-    a[0] = y_shape-1
-    d[0] = y_shape-1
+    if update_y:
+        a[0] = y_shape-1
+        d[0] = y_shape-1
+    else:
+        c[1] = x_shape-1
+        d[1] = x_shape-1
+
     polygon_corner_dict[i] = [a[:], b[:], c[:], d[:]]
 
     if plot: