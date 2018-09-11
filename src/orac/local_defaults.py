eb6e68207a550fc41e0893e34474e25fbf2d5bbd
Daniel Fisher <danielfisher0@gmail.com>
2018-09-11 13:43:57 +0100

    removed retrieval settings for now

--- src/orac/local_defaults.py
+++ src/orac/local_defaults.py
@@ -89,115 +89,115 @@ extra_lines = {
 
 # ===== DEFAULT RETRIEVAL SETTINGS =====
 
-retrieval_settings = {}
-
-# Permute a set of standard cloud retrievals over each sensor
-cld_retrievals = {
-    "wat_cld" : "--phase WAT --ret_class ClsCldWat --approach AppCld1L "
-                "--sub_dir cld",
-    "ice_cld" : "--phase ICE --ret_class ClsCldIce --approach AppCld1L "
-                "--sub_dir cld",
-    "wat_ice" : "--phase WAT --ret_class ClsCldWat --approach AppCld2L "
-                "--multilayer ICE ClsCldIce --sub_dir cld",
-#    "ice_ice" : "--phase ICE --ret_class ClsCldIce --approach AppCld2L "
-#                "--multilayer ICE ClsCldIce --sub_dir cld"
-}
-cld_channels = {
-    "AATSR" : "--use_channels 2 3 4 5 6 7",
-    "ATSR2" : "--use_channels 2 3 4 5 6 7",
-    "AVHRR" : "--use_channels 1 2 3 4 5 6",
-    "MODIS" : "--use_channels 1 2 6 20 31 32",
-    "SEVIRI" : "--use_channels 1 2 3 4 9 10",
-}
-for sensor, channels in cld_channels.items():
-    retrieval_settings[sensor + "_C"] = [
-        channels + " " + ret for ret in cld_retrievals.values()
-    ]
-
-# Permute dual-view aerosol retrievals
-aer_phases = range(70, 80)
-aer_dual_retrievals = {
-    "aer_ox" : "--phase A{:02d} --ret_class ClsAerOx --approach AppAerOx "
-               "--no_land --sub_dir sea",
-    "aer_sw" : "--phase A{:02d} --ret_class ClsAerSw --approach AppAerSw "
-               "--no_sea --sub_dir land",
-}
-aer_dual_channels = {
-    "AATSR" : "--use_channels 1 2 3 4 8 9 10 11",
-    "ATSR2" : "--use_channels 1 2 3 4 8 9 10 11",
-}
-for sensor, channels in aer_dual_channels.items():
-    retrieval_settings[sensor + "_A"] = [
-        channels + " " + ret.format(phs)
-        for ret in aer_dual_retrievals.values() for phs in aer_phases
-    ]
-
-# Permute single-view aerosol retrievals
-aer_single_retrievals = {
-    "aer_o1" : "--phase A{:02d} --ret_class ClsAerOx --approach AppAerO1 "
-               "--no_land --sub_dir sea",
-}
-aer_single_channels = {
-    "AVHRR" : "--use channels 1 2 3",
-    "MODIS" : "--use_channels 4 1 2 6",
-    "SEVIRI" : "--use_channels 1 2 3",
-}
-for sensor, channels in aer_single_channels.items():
-    retrieval_settings[sensor + "_A"] = [
-        channels + " " + ret.format(phs)
-        for ret in aer_single_retrievals.values() for phs in aer_phases
-    ]
-
-# Joint aerosol-cloud retrievals
-for sensor, channels in cld_channels.items():
-    retrieval_settings[sensor + "_J"] = [
-        channels + " " + ret for ret in cld_retrievals.values()
-    ]
-for sensor, channels in aer_dual_channels.items():
-    retrieval_settings[sensor + "_J"].extend([
-        channels + " " + ret.format(phs)
-        for ret in aer_dual_retrievals.values() for phs in aer_phases
-    ])
-for sensor, channels in aer_single_channels.items():
-    retrieval_settings[sensor + "_J"].extend([
-        channels + " " + ret.format(phs)
-        for ret in aer_single_retrievals.values() for phs in aer_phases
-    ])
-
-# Default to cloud retrievals
-for sensor in cld_channels.keys():
-    retrieval_settings[sensor] = retrieval_settings[sensor + "_C"]
-
-
-# ===== DEFAULT CHANNELS FOR EACH SENSOR =====
-
-channels = {
-    "AATSR" : (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
-    "ATSR2" : (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
-    "AVHRR" : (1, 2, 3, 4, 5, 6),
-    "MODIS" : (1, 2, 6, 20, 31, 32),
-    "SEVIRI" : (1, 2, 3, 4, 9, 10),
-}
-
-
-# ===== REGRESSION TEST SETTINGS =====
-
-# Fields to ignore during regression testing
-atts_to_ignore = ('L2_Processor_Version', 'Production_Time', 'File_Name')
-vars_to_accept = ('costjm', 'costja', 'niter')
-
-# Tollerances in regression testing
-rtol = 1e-7 # Maximum permissable relative difference
-atol = 1e-7 # Maximum permissable absolute difference
-
-# Filters to apply regression test warnings
-# (see https://docs.python.org/2/library/warnings.html#the-warnings-filter)
-warn_filt = {
-    'FieldMissing'    : 'once',
-    'InconsistentDim' : 'error',
-    'RoundingError'   : 'once',
-    'Acceptable'      : 'once',
-}
+# retrieval_settings = {}
+#
+# # Permute a set of standard cloud retrievals over each sensor
+# cld_retrievals = {
+#     "wat_cld" : "--phase WAT --ret_class ClsCldWat --approach AppCld1L "
+#                 "--sub_dir cld",
+#     "ice_cld" : "--phase ICE --ret_class ClsCldIce --approach AppCld1L "
+#                 "--sub_dir cld",
+#     "wat_ice" : "--phase WAT --ret_class ClsCldWat --approach AppCld2L "
+#                 "--multilayer ICE ClsCldIce --sub_dir cld",
+# #    "ice_ice" : "--phase ICE --ret_class ClsCldIce --approach AppCld2L "
+# #                "--multilayer ICE ClsCldIce --sub_dir cld"
+# }
+# cld_channels = {
+#     "AATSR" : "--use_channels 2 3 4 5 6 7",
+#     "ATSR2" : "--use_channels 2 3 4 5 6 7",
+#     "AVHRR" : "--use_channels 1 2 3 4 5 6",
+#     "MODIS" : "--use_channels 1 2 6 20 31 32",
+#     "SEVIRI" : "--use_channels 1 2 3 4 9 10",
+# }
+# for sensor, channels in cld_channels.items():
+#     retrieval_settings[sensor + "_C"] = [
+#         channels + " " + ret for ret in cld_retrievals.values()
+#     ]
+#
+# # Permute dual-view aerosol retrievals
+# aer_phases = range(70, 80)
+# aer_dual_retrievals = {
+#     "aer_ox" : "--phase A{:02d} --ret_class ClsAerOx --approach AppAerOx "
+#                "--no_land --sub_dir sea",
+#     "aer_sw" : "--phase A{:02d} --ret_class ClsAerSw --approach AppAerSw "
+#                "--no_sea --sub_dir land",
+# }
+# aer_dual_channels = {
+#     "AATSR" : "--use_channels 1 2 3 4 8 9 10 11",
+#     "ATSR2" : "--use_channels 1 2 3 4 8 9 10 11",
+# }
+# for sensor, channels in aer_dual_channels.items():
+#     retrieval_settings[sensor + "_A"] = [
+#         channels + " " + ret.format(phs)
+#         for ret in aer_dual_retrievals.values() for phs in aer_phases
+#     ]
+#
+# # Permute single-view aerosol retrievals
+# aer_single_retrievals = {
+#     "aer_o1" : "--phase A{:02d} --ret_class ClsAerOx --approach AppAerO1 "
+#                "--no_land --sub_dir sea",
+# }
+# aer_single_channels = {
+#     "AVHRR" : "--use channels 1 2 3",
+#     "MODIS" : "--use_channels 4 1 2 6",
+#     "SEVIRI" : "--use_channels 1 2 3",
+# }
+# for sensor, channels in aer_single_channels.items():
+#     retrieval_settings[sensor + "_A"] = [
+#         channels + " " + ret.format(phs)
+#         for ret in aer_single_retrievals.values() for phs in aer_phases
+#     ]
+#
+# # Joint aerosol-cloud retrievals
+# for sensor, channels in cld_channels.items():
+#     retrieval_settings[sensor + "_J"] = [
+#         channels + " " + ret for ret in cld_retrievals.values()
+#     ]
+# for sensor, channels in aer_dual_channels.items():
+#     retrieval_settings[sensor + "_J"].extend([
+#         channels + " " + ret.format(phs)
+#         for ret in aer_dual_retrievals.values() for phs in aer_phases
+#     ])
+# for sensor, channels in aer_single_channels.items():
+#     retrieval_settings[sensor + "_J"].extend([
+#         channels + " " + ret.format(phs)
+#         for ret in aer_single_retrievals.values() for phs in aer_phases
+#     ])
+#
+# # Default to cloud retrievals
+# for sensor in cld_channels.keys():
+#     retrieval_settings[sensor] = retrieval_settings[sensor + "_C"]
+#
+#
+# # ===== DEFAULT CHANNELS FOR EACH SENSOR =====
+#
+# channels = {
+#     "AATSR" : (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
+#     "ATSR2" : (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
+#     "AVHRR" : (1, 2, 3, 4, 5, 6),
+#     "MODIS" : (1, 2, 6, 20, 31, 32),
+#     "SEVIRI" : (1, 2, 3, 4, 9, 10),
+# }
+#
+#
+# # ===== REGRESSION TEST SETTINGS =====
+#
+# # Fields to ignore during regression testing
+# atts_to_ignore = ('L2_Processor_Version', 'Production_Time', 'File_Name')
+# vars_to_accept = ('costjm', 'costja', 'niter')
+#
+# # Tollerances in regression testing
+# rtol = 1e-7 # Maximum permissable relative difference
+# atol = 1e-7 # Maximum permissable absolute difference
+#
+# # Filters to apply regression test warnings
+# # (see https://docs.python.org/2/library/warnings.html#the-warnings-filter)
+# warn_filt = {
+#     'FieldMissing'    : 'once',
+#     'InconsistentDim' : 'error',
+#     'RoundingError'   : 'once',
+#     'Acceptable'      : 'once',
+# }
 
 
 # ===== BATCH PROCESSING SETTINGS =====
