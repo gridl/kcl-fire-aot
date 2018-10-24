f2392ce1d2d1aa0fe499598a1ee21de435b20e81
dabillox <danielfisher0@gmail.com>
2018-10-24 10:30:53 +0100

    added in time subsetting to gif generation

--- src/visualization/himawari_gif_generator.py
+++ src/visualization/himawari_gif_generator.py
@@ -9,11 +9,18 @@ def main():
 
     path_to_images = '/Users/dnf/Downloads/him_gif_data/'
     filenames = np.array(os.listdir(path_to_images))
+
+    # first restrict to only valid data
     times = np.array([int(re.search("[_][0-9]{4}[_]", f).group().replace("_", "")) for f in filenames])
-    print times
     time_mask = (times > 1100) & (times < 2300)
-    print time_mask
     filenames = filenames[~time_mask]
+
+    # next get time subset
+    min_time = datetime.datetime(2015, 9, 23)
+    max_time = datetime.datetime(2015, 9, 30)
+    times = [datetime.strptime(re.search("[0-9]{8}[_][0-9]{4}", f).group(), '%Y%m%d_%H%M') for f in filenames]
+    time_mask = [min_time < t < max_time for t in times]
+    filenames = filenames[time_mask]
     print filenames
 
     with imageio.get_writer('/Users/dnf/Downloads/him_gif/mov.gif', mode='I') as writer:
